780148c6c2f86d910faa01a972c4433c109a1168
Siddhant356 <siddhantshukla356@gmail.com>
2020-05-17 14:10:29 +0530

    Motion detection Complete

--- smoothing_and_blurring_images.py
+++ smoothing_and_blurring_images.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-img = cv2.imread('data/lena.jpg')
+img = cv2.imread('data/messi5.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 kernel = np.ones((5,5), np.float32)/25
